8b0dd978a08c31251549aa8cbf5b2980a251aee0
Susritha <susrithasabbini@gmail.com>
2024-10-09 18:10:48 +0530

    Added authentication using JWT

--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const { connectDB } = require("./db/connectDB");
-const { router } = require("./router/items");
 const rfs = require("rotating-file-stream");
 const morgan = require("morgan");
 
+const { router } = require("./router/items");
+const authRouter = require("./router/authRoute");
+
 const app = express();
 app.use(express.json());
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const accessLogStream = rfs.createStream("application.log", {
   interval: "1d", // rotate daily
@@ -16,10 +24,16 @@ const accessLogStream = rfs.createStream("application.log", {
 });
 
 app.use(morgan("combined", { stream: accessLogStream }));
+app.use(cookieParser(process.env.JWT_SECRET)); // access to cookies in req.signedCookies (for more security in production)
 
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use("/auth", authRouter);
 app.use("/", router);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
