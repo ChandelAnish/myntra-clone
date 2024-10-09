641e78806ecd1d0261fab221279e41a45b1fbcf5
Susritha <susrithasabbini@gmail.com>
2024-10-09 17:37:25 +0530

    Server: Added log streams

--- server/app.js
+++ server/app.js
@@ -1,24 +1,36 @@
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const { connectDB } = require("./db/connectDB");
+const { router } = require("./router/items");
+const rfs = require("rotating-file-stream");
+const morgan = require("morgan");
+
 const app = express();
 app.use(express.json());
-const cors = require('cors');
+
+const accessLogStream = rfs.createStream("application.log", {
+  interval: "1d", // rotate daily
+  path: "./logs",
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
 app.use(cors());
-const { connectDB } = require('./DB/connectDB');
-const {router} = require('./router/items')
-require('dotenv').config();
-
-app.use('/',router);
-
+app.use(morgan("dev"));
+app.use("/", router);
 
 const port = process.env.PORT || 5000;
+
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => {
-            console.log('server connected to PORT 5000 and DB')
-        })
-    } catch (error) {
-        console.log("error occurred : ",error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log("server connected to PORT 5000 and DB");
+    });
+  } catch (error) {
+    console.log("error occurred : ", error);
+  }
+};
 start();
