57464860ebd19a8ef793d49b78f8f9c189d02d1e
Suresh <sureshb9a11460@gmail.com>
2024-10-17 16:00:34 +0530

    add to bag feature added

--- client/src/components/SingleProduct.jsx
+++ client/src/components/SingleProduct.jsx
@@ -8,6 +8,7 @@ import { BsTruck } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { setProduct } from "../store/productSlice";
 import { loadingStatusSliceAction } from "../store/LoadingStatus";
+import { bagItemsAction } from "../store/bagSlice";
 import LoadingSpinner from "./loadingSpinner";
 
 const SingleProduct = () => {
@@ -15,14 +16,14 @@ const SingleProduct = () => {
   const { id } = useParams();
 
   const dispatch = useDispatch();
-  const loadingStatus = useSelector(store => store.loadingStatus);
+  const loadingStatus = useSelector((store) => store.loadingStatus);
   const singleItem = useSelector((state) => state.product.item);
-
+  
   useEffect(() => {
     async function fetchingItems() {
       dispatch(loadingStatusSliceAction.startloading());
       try {
-        const url = `http://localhost:5000/get-product/${id}`;
+        const url = `${import.meta.env.VITE_URL}/${id}`;
         const response = await fetch(url);
         const data = await response.json();
         if (data.success) {
@@ -44,91 +45,99 @@ const SingleProduct = () => {
     return <div>Loading...</div>;
   }
 
+  const handleAddToBag = (item) => {
+    dispatch(bagItemsAction.addToBag(item));
+  };
+
   return (
     <>
-   {(loadingStatus.loading) ? <LoadingSpinner/> : <div className="container">
-      <div className="flex-container">
-        <div className="image-container">
-          <img src={singleItem.image} alt={singleItem.item_name} />
-        </div>
-        <div className="details-container">
-          <h2>{singleItem.company}</h2>
-          <p className="item-name">{singleItem.item_name}</p>
-          <p className="rating-stars">
-            {singleItem.rating.stars} <FaStar className="star" />{" "}
-            <span className="divide"> | </span> {singleItem.rating.count}{" "}
-            Ratings
-          </p>
-
-          <hr />
-
-          <div className="price-container">
-            <div className="price">
-              <span className="current-price">₹{singleItem.current_price}</span>
-              <p style={{ color: "#777" }}>
-                ₹MRP
-                <span className="original-price">
-                  {singleItem.original_price}
-                </span>
+      {loadingStatus.loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="container">
+          <div className="flex-container">
+            <div className="image-container">
+              <img src={singleItem.image} alt={singleItem.item_name} />
+            </div>
+            <div className="details-container">
+              <h2>{singleItem.company}</h2>
+              <p className="item-name">{singleItem.item_name}</p>
+              <p className="rating-stars">
+                {singleItem.rating.stars} <FaStar className="star" />{" "}
+                <span className="divide"> | </span> {singleItem.rating.count}{" "}
+                Ratings
               </p>
-              <span className="discount">
-                ({singleItem.discount_percentage}% OFF)
-              </span>
-            </div>
-            <p>inclusive of all taxes</p>
-          </div>
-          <div>
-            <div className="size-text">
-              <p>SELECT SIZE</p>{" "}
-              <p className="size-chart">SIZE CHART {" > "}</p>
-            </div>
-            <div className="size-options">
-              <button>{singleItem.size}</button>
-              <button>40</button>
-              <button>42</button>
-              <button>44</button>
-            </div>
-          </div>
-          <div className="btn-container">
-            <button className="add-to-bag">
-              <span> ADD TO BAG </span>
-              <HiShoppingBag className="btn-icon" />{" "}
-            </button>
-            <button className="wishlist">
-              <span> WISHLIST </span>
-              <CgHeart className="btn-icon" />
-            </button>
-          </div>
-          <hr />
 
-          <div className="delivery-options">
-            <h2>
-              DELIVERY OPTIONS <BsTruck className="truck-icon" />
-            </h2>
-            <form className="pincode-wrapper">
-              <input
-                type="text"
-                className="pincode-input"
-                placeholder="Enter pincode"
-                required
-              />
-              <button type="submit" className="check-btn">
-                Check
-              </button>
-            </form>
-            <p className="delivery-info">
-              Please enter PIN code to check delivery time & Pay on Delivery
-              Availability
-            </p>
-            <ul className="benefits-list">
-              <li>100% Original Products</li>
-              <li>Pay on delivery might be available</li>
-              <li>Easy 14 days returns and exchanges</li>
-            </ul>
+              <hr />
+
+              <div className="price-container">
+                <div className="price">
+                  <span className="current-price">
+                    ₹{singleItem.current_price}
+                  </span>
+                  <p style={{ color: "#777" }}>
+                    ₹MRP
+                    <span className="original-price">
+                      {singleItem.original_price}
+                    </span>
+                  </p>
+                  <span className="discount">
+                    ({singleItem.discount_percentage}% OFF)
+                  </span>
+                </div>
+                <p>inclusive of all taxes</p>
+              </div>
+              <div>
+                <div className="size-text">
+                  <p>SELECT SIZE</p>{" "}
+                  <p className="size-chart">SIZE CHART {" > "}</p>
+                </div>
+                <div className="size-options">
+                  <button>{singleItem.size}</button>
+                </div>
+              </div>
+              <div className="btn-container">
+                <button className="add-to-bag" onClick={() => handleAddToBag(singleItem)}>
+                  <span> ADD TO BAG </span>
+                  <HiShoppingBag className="btn-icon" />{" "}
+                </button>
+                <button className="wishlist">
+                  <span> WISHLIST </span>
+                  <CgHeart className="btn-icon" />
+                </button>
+              </div>
+              <hr />
+
+              <div className="delivery-options">
+                <h2>
+                  DELIVERY OPTIONS <BsTruck className="truck-icon" />
+                </h2>
+                <form className="pincode-wrapper">
+                  <input
+                    type="text"
+                    className="pincode-input"
+                    placeholder="Enter pincode"
+                    required
+                  />
+                  <button type="submit" className="check-btn">
+                    Check
+                  </button>
+                </form>
+                <p className="delivery-info">
+                  Please enter PIN code to check delivery time & Pay on Delivery
+                  Availability
+                </p>
+                <ul className="benefits-list">
+                  <li>100% Original Products</li>
+                  <li>Pay on delivery might be available</li>
+                  <li>Easy 14 days returns and exchanges</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>}</>
+      )}
+    </>
   );
 };
 
